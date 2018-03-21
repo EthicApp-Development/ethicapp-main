518777c00bc634f020665d4440c25bd734c7bb83
Sergio-P <elxsergio@gmail.com>
2018-03-21 13:05:07 -0300

    Bug fix

--- public/js/admin-ctrl.js
+++ public/js/admin-ctrl.js
@@ -1329,7 +1329,7 @@ adpp.controller("DashboardController", function ($scope, $http, $timeout, $uibMo
                             data.title = self.flang("answerOf") + " " + self.leaderTeamStr[uid];
                             data.content = self.flang("question") + ":\n" + qstxt + "\n\n" + self.flang("answer") + ":\n" + alt + "\n\n";
                             res.forEach(r => {
-                                data.content += self.flang("comment") + " " + r.uname + ":\n" + r.comment + "\n";
+                                data.content += self.flang("comment") + " " + r.uname + ":\n" + ((r.comment) ? r.comment : "") + "\n";
                                 if (r.confidence) {
                                     data.content += self.flang("confidenceLevel") + ": " + r.confidence + "%\n";
                                 }
