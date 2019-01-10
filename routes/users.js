87fde206a1111edfe06d4ca64edd08b86ba6c2d2
Sergio-P <elxsergio@gmail.com>
2019-01-10 11:15:01 -0300

    Add password reset

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,12 @@ let router = express.Router();
 let rpg = require("../modules/rest-pg");
 let pass = require("../modules/passwords");
 let crypto = require("crypto");
+let mailer = require("nodemailer");
+
+let mailserv = mailer.createTransport({
+    sendmail: true,
+    newline: 'unix'
+});
 
 router.get('/login', (req, res) => {
     res.render('login', {rc: req.query.rc});
@@ -93,6 +99,50 @@ router.post("/update-lang", rpg.singleSQL({
     sesReqData: ["uid"],
     postReqData: ["lang"],
     sqlParams: [rpg.param("post", "lang"), rpg.param("ses", "uid")]
+}));
+
+router.post("/resetpassword", rpg.execSQL({
+    dbcon: pass.dbcon,
+    sql: "insert into pass_reset(mail, token, ctime) values ($1,$2,now())",
+    postReqData: ["mail"],
+    onStart: (ses, data, calc) => {
+        let n = ~~(Math.random()*32768);
+        calc.token = "" + n + crypto.createHash('md5').update(data.user).digest('hex');
+        let mailopts = {
+            from: "noreply@saduewa.dcc.uchile.cl",
+            to: data.user,
+            subject: "Readings: Recuperación de Contraseña",
+            text: "Puedes recuperar y cambiar la contraseña de tu cuenta en Readings en el siguiente link: \n " +
+                "https://saduewa.dcc.uchile.cl:8888/readings/new-pass/" + calc.token + "\n\nReadings."
+        };
+        mailserv.sendMail(mailopts, function(err,info){
+            if(!err){
+                console.log("Mail sent");
+            }
+        });
+    },
+    sqlParams: [rpg.param("post", "user"), rpg.param("calc", "token")],
+    onEnd: (req, res) => {
+        res.redirect("login?rc=3");
+    }
+}));
+
+router.get("/new-pass/:token", (req, res) => {
+    res.render("newpass", {token: req.params.token});
+});
+
+router.post("/newpassword", rpg.execSQL({
+    dbcon: pass.dbcon,
+    sql: "update users as u set pass = $1 from pass_reset as r where r.token = $2 and r.mail = u.mail",
+    postReqData: ["token", "pass"],
+    onStart: (ses, data, calc) => {
+        if (data.pass.length < 5) return "select 1";
+        calc.passcr = crypto.createHash('md5').update(data.pass).digest('hex');
+    },
+    sqlParams: [rpg.param("calc", "passcr"), rpg.param("post", "token")],
+    onEnd: (req, res) => {
+        res.redirect("login?rc=4");
+    }
 }));
 
 module.exports = router;
