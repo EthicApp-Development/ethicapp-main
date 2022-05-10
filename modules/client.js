60390932800072114e2453441a29a29b3aefaf61
Raimundo Marin <raimundomarin@Raimundos-MacBook-Pro.local>
2022-05-10 13:59:26 -0400

    client

--- modules/client.js
+++ modules/client.js
@@ -1,24 +1,34 @@
 const celery = require("celery-node");
 const client = celery.createClient("redis://localhost:6379/0", "redis://localhost:6379/0");
 
-var getDataRealTime = client.createTask('get-data-rt');
-var getDifferentialByStage = client.createTask('get-prediction-by-stage');
+
+var getDataRealTime = client.createTask('get-data-rt', queue='celery');
+var getDifferentialByStage = client.createTask('get-prediction-by-stage', queue='celery');
 let socket = require("../modules/socket.config");
 
 
-module.exports.sendDiffSelection = function (dataDiffSelection) {
-    result = getDataRealTime.delay([dataDiffSelection])
-    result.get().then(data => {
+module.exports.sendDiffSelection = async function (dataDiffSelection) {
+    let result = getDataRealTime.applyAsync([dataDiffSelection])
+    await result.get().then(data => {
         return data
-      });
+      }).catch(err => {console.log(err);});
 }
 
-module.exports.sendAllDiffselection = function (dataAllDifferential) {
-  let result = getDifferentialByStage.delay([dataAllDifferential])
-  result.get().then(data => {
+module.exports.sendAllDiffselection = async function (dataAllDifferential) {
+  let result = getDifferentialByStage.applyAsync([dataAllDifferential])
+  await result.get().then(data => {
       console.log(data);
+      if (data.hasOwnProperty("job")) {
+        data.job.map(job => {
+          console.log(job);
+    
+        })
+      } else {
       socket.dashboard(data);
+      }
       return data
-    });
+    }).catch(err => {console.log(err);});
 }
+
+
 
