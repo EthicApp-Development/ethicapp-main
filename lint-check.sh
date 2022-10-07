25213a45c7a2813cc99e5c32b0da3fb983226f2c
Ignacio F. Garcés <ifgarces@miuandes.cl>
2022-10-07 12:34:33 -0300

    Fix on StyleLint config, updated lint-check script

--- lint-check.sh
+++ lint-check.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 # --------------------------------------------------------------------------------------------------
 # Executes the configured linters for the project, checking the code base passes the code style.
-# Intended for CI.
+# Intended for CI. This script exits with code 0 only when all linters pass. The commented lines
+# below allow to automatically fix lint rule violations whenever possible (remember you could do
+# this in your vscode due the project IDE settings).
 # --------------------------------------------------------------------------------------------------
 
 set -eu
 
 echo ">>> Running SQL linter"
 sqlfluff lint ./db_config/ --dialect postgres
+# sqlfluff fix ./db_config/ --dialect postgres --force
 echo "[OK] SQLFluff pass"
 
-# Run the following for auto-fixing linting violations (in case you don't want to do it with vscode)
-#    sqlfluff fix ./db_config/ --dialect postgres # --force
-
 echo ">>> Running CSS linter"
-exit 1 #TODO
+npx stylelint public/css/*.css
+# npx stylelint --fix public/css/*.css
 echo "[OK] StyleLint pass"
 
 echo ">>> Running HTML linter"
@@ -22,7 +23,8 @@ npx htmlhint "./**/*.html"
 echo "[OK] HTMLHint pass"
 
 echo ">>> Running JavaScript linter"
-npx eslint "./**/*.js" # add the "--fix" flag for automatically correct fixable problems
+npx eslint "./**/*.js"
+# npx eslint "./**/*.js" --fix
 echo "[OK] ESLint pass"
 
 echo "[OK] Yay! Linters passed with no errors"
