6e1c10ab93abef1efcb1510f7923aa934a70a4ca
William-CarterG <80415081+William-CarterG@users.noreply.github.com>
2022-12-22 11:01:30 -0300

    Fix some errors in populate queries (#71)
    The populate _db.sql script was causing some errores due to misplaced spaces and double quotation marks. That is now fixed.

--- db_config/populate_db.sql
+++ db_config/populate_db.sql
@@ -1,19 +1,24 @@
-INSERT INTO users (name, rut, pass, mail, sex, ROLE, aprendizaje)
+INSERT INTO users(name, rut, pass, mail, sex, ROLE, aprendizaje)
 VALUES (
-    'Profesor', '12312312-3', 'profesor', 'profesor@test', 'M', 'P', "Reflexivo"
+    'Profesor', '12312312-3', 'profesor', 'profesor@test', 'M', 'P', 'Reflexivo'
 );
 
-INSERT INTO users (name, rut, pass, mail, sex, role, aprendizaje)
+INSERT INTO users(name, rut, pass, mail, sex, role, aprendizaje)
 VALUES (
-    "Alumno 1", "12121212-1", "alumno1", "alumno1@test", "M", "A", "Reflexivo"
+    'Alumno 1', '12121212-1', 'alumno1', 'alumno1@test', 'M', 'A', 'Reflexivo'
 );
 
-INSERT INTO users (name, rut, pass, mail, sex, role, aprendizaje)
+INSERT INTO users(name, rut, pass, mail, sex, role, aprendizaje)
 VALUES(
-    "Alumno 2", "11111111-1", "alumno2", "alumno2@test", "F", "A", "Teorico"
+    'Alumno 2', '11111111-1', 'alumno2', 'alumno2@test', 'F', 'A', 'Teorico'
 );
 
-INSERT INTO users (name, rut, pass, mail, sex, role, aprendizaje)
+INSERT INTO users(name, rut, pass, mail, sex, role, aprendizaje)
 VALUES(
-    "Alumno 3", "22222222-2", "alumno3", "alumno3@test", "F", "A", "Activo"
+    'Alumno 3', '22222222-2','alumno3', 'alumno3@test', 'F', 'A', 'Activo'
+);
+
+INSERT INTO users(name, rut, pass, mail, sex, role, aprendizaje)
+VALUES(
+    'MEGA CHECK TEST USER', '22222222-2','testuser', 'checkuser@test', 'F', 'P', 'Reflexivo'
 );
