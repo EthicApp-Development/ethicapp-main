1d426d13dfd130abcb33729379b9a3e3243547ca
William-CarterG <80415081+William-CarterG@users.noreply.github.com>
2022-12-22 17:25:59 -0300

    DB CREATE script
    DB create script finished with all the necessary 'customization' options required.

--- db_config/create_db.sql
+++ db_config/create_db.sql
@@ -1,10 +1,9 @@
--- Database: doccollab
+-- Database: :db_name
+-- DROP DATABASE IF EXISTS :db_name
 
--- DROP DATABASE IF EXISTS doccollab;
-
-CREATE DATABASE doccollabtest
+CREATE DATABASE :db_name
     WITH
-    OWNER = postgres
+    OWNER = :pg_user
     ENCODING = 'UTF8'
     --LC_COLLATE = 'en_US.utf8'
     --LC_CTYPE = 'en_US.utf8'
@@ -12,14 +11,10 @@ CREATE DATABASE doccollabtest
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
-GRANT TEMPORARY, CONNECT ON DATABASE doccollabtest TO PUBLIC;
+GRANT TEMPORARY, CONNECT ON DATABASE :db_name TO PUBLIC;
 
-GRANT ALL ON DATABASE doccollabtest TO app;
+GRANT ALL ON DATABASE :db_name TO :pg_user;
 
-GRANT ALL ON DATABASE doccollabtest TO postgres;
+GRANT ALL ON DATABASE :db_name TO postgres;
 
-CREATE TABLE IF NOT EXISTS test(
-    id serial,
-    sesid integer,
-    PRIMARY KEY(id)
-);
+CREATE ROLE :pg_user WITH PASSWORD :pg_password;
--- db_config/init_db.sh
+++ db_config/init_db.sh
@@ -19,16 +19,18 @@ PG_PASSSWORD=${3:-$PGPASSWORD}    #Password given when created the server in pgA
 
 #Creates the database $DB_NAME in server.
 CREATE_DB_FILE=db_config/create_db.sql
-psql -U $PG_USER -a -f $CREATE_DB_FILE -h $PGHOST -p $PGPORT
+psql -U $PG_USER -a -f $CREATE_DB_FILE -h $PGHOST -p $PGPORT -v db_name=$DB_NAME -v pg_user=$PG_USER -v pg_password=$PG_PASSSWORD
 
 
 #Creates connection string for new database.
-export DBCON=${DBCON-postgresql://$PG_USER:$PG_PASSSWORD@$PGHOST:$PGPORT/$DB_NAME}
+connectionString=postgresql://$PG_USER:$PG_PASSSWORD@$PGHOST:$PGPORT/$DB_NAME
+
+
 
 # Creates and implements the schema for db $DB_NAME.
 for f in ./db_config/create_sql_schema/* ; do
   case "$f" in
-    *.sql) psql $DBCON --file=${f} ;;
+    *.sql) psql $connectionString --file=${f} ;;
 
   esac
   echo
@@ -41,11 +43,12 @@ psql -U $PG_USER -a -f $POPULATE_MYDB -h $PGHOST -p $PGPORT -d $DB_NAME
 
 
 #Once the database it's fully created, it exports the connection string to the rest of the app.
+export DBCON==${DBCON-postgresql://$PG_USER:$PG_PASSSWORD@postgres/$DB_NAME}
 DBCON=$DBCON node modules/passwords.js
 
-<<comment
-For uploading db dump into dockerized container pgadmin address
-pg_dump --attribute-inserts --dbname=doccollabtest --username=postgres --password --host=localhost --file=ethicapp-postgres-$(date +%F).sql
-psql postgresql://app:app2020@localhost:5432/doccollab --file=ethicapp-postgres-$(date +%F).sql --quiet
-comment
 
+<<comment
+#For uploading db dump into dockerized container pgadmin address
+pg_dump --attribute-inserts --dbname=doccollab --username=postgres --password --host=localhost --port=5433 --file=ethicapp-postgres-$(date +%F).sql
+psql $connectionString --file=ethicapp-postgres-$(date +%F).sql --quiet
+comment
