f163aa2600f22cdf989e3b71f689d960f5509a8d
William-CarterG <80415081+William-CarterG@users.noreply.github.com>
2023-01-03 15:49:36 -0300

    Solved password encrypting problem (#71)
    Solved an issue where the testing users couldn't login, because their passwords weren't properly encrypted.

--- db_config/populate_db.sql
+++ db_config/populate_db.sql
@@ -1,24 +1,26 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 INSERT INTO users(name, rut, pass, mail, sex, ROLE, aprendizaje)
 VALUES (
-    'Profesor', '12312312-3', 'profesor', 'profesor@test', 'M', 'P', 'Reflexivo'
+    'Profesor', '12312312-3', md5('profesor'), 'profesor@test', 'M', 'P', 'Reflexivo'
 );
 
 INSERT INTO users(name, rut, pass, mail, sex, role, aprendizaje)
 VALUES (
-    'Alumno 1', '12121212-1', 'alumno1', 'alumno1@test', 'M', 'A', 'Reflexivo'
+    'Alumno 1', '12121212-1', md5('alumno1'), 'alumno1@test', 'M', 'A', 'Reflexivo'
 );
 
 INSERT INTO users(name, rut, pass, mail, sex, role, aprendizaje)
 VALUES(
-    'Alumno 2', '11111111-1', 'alumno2', 'alumno2@test', 'F', 'A', 'Teorico'
+    'Alumno 2', '11111111-1', md5('alumno2'), 'alumno2@test', 'F', 'A', 'Teorico'
 );
 
 INSERT INTO users(name, rut, pass, mail, sex, role, aprendizaje)
 VALUES(
-    'Alumno 3', '22222222-2','alumno3', 'alumno3@test', 'F', 'A', 'Activo'
+    'Alumno 3', '22222222-2',md5('alumno3'), 'alumno3@test', 'F', 'A', 'Activo'
 );
 
 INSERT INTO users(name, rut, pass, mail, sex, role, aprendizaje)
 VALUES(
-    'MEGA CHECK TEST USER', '22222222-2','testuser', 'checkuser@test', 'F', 'P', 'Reflexivo'
+    'MEGA CHECK TEST USER', '22222222-2', md5('testuser'), 'checkuser@test', 'F', 'P', 'Reflexivo'
 );
