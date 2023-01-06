4a888eba08e7971ea53fb462c7bdd84d438aa8e0
Ignacio F. Garcés <ifgarces@miuandes.cl>
2023-01-06 02:33:05 -0300

    Added server config for pgadmin (closes #79)
    Performed some small refactorings as well

--- postgres-db/entrypoint.sh
+++ postgres-db/entrypoint.sh
@@ -46,9 +46,9 @@
 chown -R postgres:postgres ${PGDATA}
 chmod -R 0700 ${PGDATA}
 
-su postgres -c "/usr/lib/postgresql/${POSTGRES_VERSION_MAJOR}/bin/postgres \
+su postgres -c "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/postgres \
     -d ${POSTGRES_LOG_LEVEL} \
     -p ${PGPORT} \
     -h 0.0.0.0 \
     -D ${PGDATA} \
-    -c config_file=/etc/postgresql/${POSTGRES_VERSION_MAJOR}/main/postgresql.conf"
+    -c config_file=/etc/postgresql/${POSTGRES_VERSION}/main/postgresql.conf"
