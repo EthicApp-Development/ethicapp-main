8eda38b747392443e5d42dad9503a96f07299a67
Ignacio F. Garcés <ifgarces@miuandes.cl>
2023-01-06 00:30:00 -0300

    Enhanced dockerized config of Postgres (#80)
    [Backup commit]
    
    - Properly configured external connections for postgres container
    - Other settings changed at postgres container
    - Added custom logging level for postgres container

--- postgres-db/entrypoint.sh
+++ postgres-db/entrypoint.sh
@@ -1,14 +1,54 @@
 #!/bin/bash -exu
 
-# Handling permissions due Docker volume
+# ---
+# /etc/init.d/postgresql start
+
+# # Creating root user, just for avoiding postgres logging complaints during healthchecks
+# su postgres -c "psql --command=\"\
+#     CREATE ROLE root WITH \
+#     NOSUPERUSER \
+#     LOGIN; \
+# \"" \
+# && su postgres -c "psql --command=\"\
+#     CREATE DATABASE root WITH \
+#     OWNER root; \
+# \""
+# # Other setup
+# su postgres -c "psql --command=\"
+#     CREATE ROLE app WITH
+#     NOSUPERUSER
+#     LOGIN
+#     PASSWORD 'app2020'
+#     CREATEDB
+#     NOCREATEROLE;
+# \""
+# su postgres -c "psql --command=\"
+#     CREATE DATABASE doccollab WITH
+#     OWNER app
+#     ENCODING 'UTF-8';
+# \""
+# su postgres -c "psql postgresql://app:app2020@localhost:5432/doccollab \
+#     --file=/code/ethicapp-postgres-2022-12-06.sql \
+#     --quiet"
+# PGPASSWORD=app2020 su postgres -c "pg_restore \
+#     --host=localhost \
+#     --port=5432 \
+#     --username=app \
+#     --no-password \
+#     --no-owher \
+#     --schema-only \
+#     /code/ethicapp-postgres-2022-12-06.sql"
+
+# /etc/init.d/postgresql stop
+# ---
+
+# Handling permissions due Postgres security constraints
 chown -R postgres:postgres ${PGDATA}
-chmod -R 700 ${PGDATA}
+chmod -R 0700 ${PGDATA}
 
-cp -fr /tmp/data/* /var/lib/postgresql/data
-
-# su postgres -c "/usr/lib/postgresql/${POSTGRES_VERSION_MAJOR}/bin/postgres \
-#     -d ${POSTGRES_LOG_LEVEL} \
-#     -p ${POSTGRES_PORT} \
-#     -D ${PGDATA} \
-#     -h 0.0.0.0 \
-#     -c config_file=/etc/postgresql/${POSTGRES_VERSION_MAJOR}/main/postgresql.conf"
+su postgres -c "/usr/lib/postgresql/${POSTGRES_VERSION_MAJOR}/bin/postgres \
+    -d ${POSTGRES_LOG_LEVEL} \
+    -p ${PGPORT} \
+    -h 0.0.0.0 \
+    -D ${PGDATA} \
+    -c config_file=/etc/postgresql/${POSTGRES_VERSION_MAJOR}/main/postgresql.conf"
