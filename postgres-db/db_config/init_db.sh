48d7a55ad45139047e28b5c6911341cb75bb8734
William-CarterG <80415081+William-CarterG@users.noreply.github.com>
2023-03-21 00:16:30 -0300

    Update after main branch merge

--- postgres-db/db_config/init_db.sh
+++ postgres-db/db_config/init_db.sh
@@ -18,11 +18,53 @@ PG_PORT=${4:-$PGPORT}
 
 
 #Creates the database $DB_NAME in server.
+<<create-db 
 CREATE_DB_FILE=db_config/create_db.sql
 psql -U $PGUSER -a -f $CREATE_DB_FILE -h $PGHOST -p $PG_PORT -v db_name=$DB_NAME -v \
 pg_user=$PG_USER -v pg_password=$PG_PASSSWORD
+create-db
+
+#               README ISSUE 71 TIPS
+#Possible alternative method: Copy each file into docker world and execute it from inside container. 
+#Problem: Makes customization difficult. Perhaps user can manually create db and postgres user, and thrn
+#run this script to create schema.
+#docker exec -it ethicapp-postgres bash -c 'psql postgresql://test:test@localhost:5432/test --file=${f}'
+#docker cp db_config/create_db.sql ethicapp-postgres:create_db.sql
+#docker exec ethicapp-postgres psql -U postgres -f create_db.sql
 
 
+
+#Other alternative: This copy every file inside the /docker-entrypoint-initdb.d/ folder (in the container).
+#This folder, according to docker documentation, "after the entrypoint calls initdb to create the default postgres 
+#user and database, it will run any *.sql files, run any executable *.sh scripts, and source any 
+#non-executable *.sh scripts found in that directory to do further initialization before starting the service."
+#which makes this approach very good. Also, only runs "if you start the container with a data directory that is empty; 
+#any pre-existing database will be left untouched on container startup."
+#However, hasn't worked properly during my testing...
+docker cp create-sql-schema/. ethicapp-postgres:/docker-entrypoint-initdb.d/
+
+#If you try this method remember that, after you make the first 'docker cp' line work
+#you will need to move both the "create_db.sql" and the "populate_db.sql" file inside the 
+#/docker-entrypoint-initdb.d/ folder for full implementation. Also, keep in mind that 
+#the "create_db.sql" currently works by receiving parameters from the command line for credentials, which 
+#will not swork after implementing this solution. 
+#This can be replaced by having each developer manually create his own enviroment values for credentials, 
+#and then replacing the command line parameters inside the "create_db.sql" file for this env values.
+
+<<possibly-useful-commands
+docker exec -it ethicapp-postgres bash
+su -c psql postgres
+CREATE ROLE test WITH PASSWORD 'test'; ALTER ROLE test WITH PASSWORD 'test';
+ALTER ROLE "test" WITH LOGIN;
+CREATE DATABASE test_db WITH OWNER test;
+postgresql://test:test@localhost:5432/test_db
+#Creates the database $DB_NAME in server.
+CREATE_DB_FILE=db_config/create_db.sql
+psql -U $PGUSER -a -f $CREATE_DB_FILE -h $PGHOST -p $PG_PORT -v db_name=$DB_NAME -v \
+pg_user=$PG_USER -v pg_password=$PG_PASSSWORD
+possibly-useful-commands
+
+<<commented-for-testing
 #Creates connection string for new database.
 connectionString=postgresql://$PG_USER:$PG_PASSSWORD@$PGHOST:$PG_PORT/$DB_NAME
 
@@ -31,7 +73,10 @@ connectionString=postgresql://$PG_USER:$PG_PASSSWORD@$PGHOST:$PG_PORT/$DB_NAME
 # Creates and implements the schema for db $DB_NAME.
 for f in ./db_config/create_sql_schema/* ; do
   case "$f" in
-    *.sql) psql $connectionString --file=${f} ;;
+    #few alternatives
+    #1option *.sql) docker cp --file=${f} ethicapp-postgres:${f}
+    #2option *.sql) docker exec psql $connectionString --file=${f} ;;
+    #3option *.sql) psql $connectionString --file=${f} ;;
   esac
   echo
 done
@@ -80,3 +125,4 @@ then
 else echo "File passwords.js doesn't exist. Ask the maintainer getting the required secret values." ;
   #[TODO] Could create here the passwords.js file, but without the secrets...
 fi
+commented-for-testing
