aec164dbc15958bde2770f73a87c6acb5b205d2a
Ignacio F. Garcés <48460500+ifgarces@users.noreply.github.com>
2023-03-25 17:29:12 -0300

    Small refactorings (#71)
    (backup commit)
    
    - Commented out problematic database seeds (for #99, temporal)
    - Other small changes

--- postgres-db/seeds/institution.sql
+++ postgres-db/seeds/institution.sql
@@ -1,3 +1,3 @@
-INSERT INTO institutions(userid, institutionname, numestudents, country, maildomains, "position")
-VALUES
-(1, 'QWERTY-institution', 999, 'qwerty', 'qwerty@test', 'qwerty');
+-- INSERT INTO institution(userid, institutionname, numestudents, country, maildomains, "position")
+-- VALUES
+-- (1, 'QWERTY-institution', 999, 'qwerty', 'qwerty@test', 'qwerty');
