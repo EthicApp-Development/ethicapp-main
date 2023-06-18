a96e8aad549d3f826f766fea094fa667bc7897f1
javiersoto99 <jisoto2@miuandes.cl>
2023-06-18 16:51:09 -0400

    Bundling and cache busting for assets (#90 & #136)

---
+++ ethicapp/frontend/assets/js/main.js
@@ -0,0 +1,14 @@
+import '../libs/ui-bootstrap-tpls-1.1.2.min.js';
+import '../libs/angular-bootstrap-multiselect.js';
+import '../libs/socket.min.js';
+import '../libs/angular-ui-notification.min.js';
+import '../libs/save-csv.min.js';
+import '../libs/angular-tablesort.min.js';
+import '../libs/angular-route.js';
+import '../libs/checklist-model.js';
+import '../libs/angular-route.js';
+import '../libs/angular-timer.min.js';
+import '../libs/d3.min.js';
+import '../libs/nv.d3.min.js';
+import '../libs/quill.min.js';
+import '../libs/katex.min.js';
