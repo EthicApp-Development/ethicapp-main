44c81bee6d0f1164132b3e5af6cf4008b7aca750
Jigracia <jigracia@miuandes.cl>
2023-09-10 21:46:52 -0300

    [Reports] Updated reports view

--- admin-panel/src/pages/SingleReport.js
+++ admin-panel/src/pages/SingleReport.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useParams} from 'react-router-dom';
 import { Line } from 'react-chartjs-2';
 import { Chart, registerables } from 'chart.js';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
+import { Container, Paper, Grid, Radio, RadioGroup, FormControlLabel, TextField, Button, FormControl, FormLabel, Typography, Box, IconButton, Hidden } from '@mui/material';
+import { SaveAlt as SaveAltIcon } from '@mui/icons-material';
 
 
 Chart.register(...registerables);
@@ -12,6 +12,7 @@ Chart.register(...registerables);
 function SingleReport() {
 
   const {reportEnum} = useParams();
+  const [showSecondBox, setShowSecondBox] = useState(false);
 
   const data = {
     labels: ['January', 'February', 'March', 'April', 'May'],
@@ -35,6 +36,13 @@ function SingleReport() {
     },
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Implement report generation logic here
+    // After generating the report, show the second box
+    setShowSecondBox(true);
+  };
+
   return(
     <Box
       component="main"
@@ -50,11 +58,84 @@ function SingleReport() {
     >
       <Toolbar />
       <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
-        <h2>Report Page for {reportEnum}</h2>
-        <h2>Show the report basic info here and the query the data with the given parameters</h2>
-        <div style={{ width: '600px', height: '600px' }}>
-          <Line data={data} options={options} />
-        </div>
+      <Typography variant="h4" gutterBottom>
+            Report Generation
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            Insert information about the specific reports that is being generated.
+          </Typography>
+        <br/>
+        {/* First Box */}
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} style={{ height: '100%' }}>
+            <form onSubmit={handleSubmit}>
+              <Box p={3}>
+                <Typography variant="h5" gutterBottom>
+                  Report Options
+                </Typography>
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={12} md={6}>
+                    <FormControl component="fieldset">
+                      <FormLabel component="legend">Select an option:</FormLabel>
+                      <RadioGroup row aria-label="report-option" name="reportOption">
+                        <FormControlLabel value="option1" control={<Radio />} label="1 Month" />
+                        <FormControlLabel value="option2" control={<Radio />} label="3 Month" />
+                        <FormControlLabel value="option3" control={<Radio />} label="6 Month" />
+                        <FormControlLabel value="option4" control={<Radio />} label="12 Month" />
+                        <FormControlLabel value="option5" control={<Radio />} label="Custom Date Range" />
+                      </RadioGroup>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={6} md={3}>
+                    <TextField
+                      id="start-date"
+                      label="Start Date"
+                      type="date"
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={6} md={3}>
+                    <TextField
+                      id="end-date"
+                      label="End Date"
+                      type="date"
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+                <br/>
+                <Button type="submit" variant="contained" color="primary">
+                  Generate Report
+                </Button>
+              </Box>
+            </form>
+          </Paper>
+        </Grid>
+
+        <br/>
+
+        {/* Second Box */}
+        {showSecondBox && (
+          <Grid item xs={12} md={6}>
+            <Paper elevation={3} style={{ height: '100%' }}>
+              <Box p={3}>
+                <IconButton color="primary" component="a">
+                  <SaveAltIcon />
+                  Download
+                </IconButton>
+                <div style={{ width: 'auto', height: 'auto', alignContent: 'center'}}>
+                  <Line data={data} options={options} />
+                </div>
+              </Box>
+            </Paper>
+          </Grid>
+        )}
       </Container>
     </Box>
   ) 
