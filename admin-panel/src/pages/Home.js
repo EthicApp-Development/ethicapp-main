d214b8d32fac5b0e1d3895a16c9ddb2f285b2a0f
Jigracia <jigracia@miuandes.cl>
2023-09-10 21:56:51 -0300

    [Home] Updated home view

--- admin-panel/src/pages/Home.js
+++ admin-panel/src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
+import { Grid, Typography, Divider, styled } from '@mui/material';
+import SchoolIcon from '@mui/icons-material/School';
+import PeopleIcon from '@mui/icons-material/People';
+import BarChartIcon from '@mui/icons-material/BarChart';
+
+// Override link styles
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit; /* Inherit text color from parent */
+`;
 
 function Home() {
+
+  const gridData = [
+    { icon: <SchoolIcon fontSize="large" />, text: 'Update institutional data', link:"institution", },
+    { icon: <PeopleIcon fontSize="large" />, text: 'See Reports', link:"reports", },
+    { icon: <BarChartIcon fontSize="large" />, text: 'Administer Users', link:"users", },
+    // Add more data as needed
+  ];
+
   return(
     <Box
       component="main"
@@ -19,8 +38,25 @@ function Home() {
     >
       <Toolbar />
       <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
-        <h2>Admin Home</h2>
-        <h2>Show general admin panel info here</h2>
+          <Typography variant="h4" gutterBottom>
+            Admin Home
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            From here you can update the data of the institution for which EthicApp operates, review reports, and manage users..
+          </Typography>
+        <br/>
+        <Grid container spacing={2}>
+          {gridData.map((item, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+              <StyledLink to={`/admin/${item.link}`} style={{ textDecoration: 'none' }}>
+                <Box display="flex" alignItems="center" border="1px solid black" padding={2}>
+                  <Box marginRight={2}>{item.icon}</Box>
+                  <Typography variant="body1">{item.text}</Typography>
+                </Box>
+              </StyledLink>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </Box>
   );
