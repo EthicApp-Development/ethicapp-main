1976e90f2c268a735155f067f1362216215532ca
Jigracia <jigracia@miuandes.cl>
2023-10-08 14:16:08 -0300

    [Js] Save graphs as PDFs

--- admin-panel/src/components/ReportGraphBox.js
+++ admin-panel/src/components/ReportGraphBox.js
@@ -1,5 +1,8 @@
+import React, { useRef } from 'react';
 import { Paper, Grid, Box, IconButton, Tooltip } from '@mui/material';
 import { SaveAlt as SaveAltIcon } from '@mui/icons-material';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 const ReportGraphBox = (props) =>{
 
@@ -8,18 +11,37 @@ const ReportGraphBox = (props) =>{
     const graph = props.graph;
     const visibility = props.visibility;
 
+    const graphRef = useRef(null);
+
+    const captureChartAsImage = () => {
+        console.log(graphRef.current)
+        if (graphRef.current) {
+        html2canvas(graphRef.current).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF('landscape'); // Set landscape orientation
+            const aspectRatio = canvas.width / canvas.height;
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = pdfWidth / aspectRatio;
+            const x = 0;
+            const y = (pdf.internal.pageSize.getHeight() - pdfHeight) / 2;
+            pdf.addImage(imgData, 'PNG', x, y, pdfWidth, pdfHeight);
+            pdf.save('chart.pdf');
+        });
+        }
+    };
+
     return<>         
         {visibility && (
             <Grid item xs={12} md={6}>
                 <Paper elevation={3} style={{ height: '100%' }}>
                     <Box p={3}>
                         <Tooltip title={translation("ReportGraphBox.downloadTooltip")}>
-                            <IconButton color="primary" component="a">
+                            <IconButton color="primary" component="a" onClick={captureChartAsImage}>
                                 <SaveAltIcon />
                                 {translation("ReportGraphBox.download")}
                             </IconButton>
                         </Tooltip>
-                        <div style={{ width: 'auto', height: 'auto', alignContent: 'center'}}>
+                        <div style={{ width: 'auto', height: 'auto', alignContent: 'center'}} ref={graphRef}>
                             {graph}
                         </div>
                     </Box>
