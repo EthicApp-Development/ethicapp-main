40d3170e028fa004c01c7102963c8ff7ce6540c4
Benjamin Gonzalez <bigonzalez@miuandes.cl>
2024-04-03 17:34:09 -0300

    [create_26] add new's columns[#296] add new's columns in table's

---
+++ ethicapp/backend/controllers/api/v2/migrations/20240331154035-create_26.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('sessions', 'current_stage', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'stages',
+        key: 'id'
+      }
+    });
+    await queryInterface.addColumn('teams', 'stageid', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'stages',
+        key: 'id'
+      }
+    });
+    await queryInterface.addColumn('actor_selection', 'stime', {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    });
+    await queryInterface.addColumn('actors', 'justified', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    });
+    await queryInterface.addColumn('stages', 'question', {
+      type: Sequelize.TEXT,
+      allowNull: true
+    });
+    await queryInterface.addColumn('stages', 'grouping', {
+      type: Sequelize.STRING(63), 
+      allowNull: true
+    });
+                    
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropAllTables();
+  }
+};
