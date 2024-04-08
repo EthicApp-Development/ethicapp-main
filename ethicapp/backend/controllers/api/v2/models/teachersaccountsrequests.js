9ce3910ae5de7f9482f0a5ccd3a72cfbb3b94b07
Benjamin Gonzalez <bigonzalez@miuandes.cl>
2024-04-07 21:38:32 -0400

    [TeacherAccountRequest] changed name table [#296]
    refactor changed name table's

---
+++ ethicapp/backend/controllers/api/v2/models/teachersaccountsrequests.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class TeacherAccountRequest extends Model {
+    static associate(models) {
+      // Aquí puedes definir las asociaciones con otros modelos si es necesario
+    }
+  }
+  TeacherAccountRequest.init({
+    name: DataTypes.TEXT,
+    rut: DataTypes.TEXT,
+    pass: DataTypes.TEXT,
+    mail: DataTypes.TEXT,
+    gender: DataTypes.CHAR(1),
+    institution: DataTypes.TEXT,
+    date: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    status: DataTypes.CHAR(1),
+    reject_reason: DataTypes.TEXT,
+    upgrade_flag: DataTypes.BOOLEAN,
+  }, {
+    sequelize,
+    modelName: 'TeacherAccountRequest',
+    tableName: 'teachers_accounts_requests',
+  });
+
+  return TeacherAccountRequest;
+};
