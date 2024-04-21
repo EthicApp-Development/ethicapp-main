7eb8cfc6d82d1b6a02a1ada79640739d9321cac9
sdiazgt <sidiaz3@miuandes.cl>
2024-04-21 18:59:15 -0400

    Se agrega llamada por medio de Socket entre callback y dashboard

--- postgres-db/schema/create_39.sql
+++ postgres-db/schema/create_39.sql
@@ -1,6 +1,6 @@
 CREATE TABLE content_analysis (
     id SERIAL PRIMARY KEY,
-    content text,
+    response_selections text,
     context text,
     sesid integer REFERENCES sessions(id),
     stage_number integer
