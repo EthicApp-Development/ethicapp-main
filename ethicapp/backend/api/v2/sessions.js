dc9fb02b0f6eeb79e675eb53baebc19783f684c6
Benjamin Gonzalez <bigonzalez@miuandes.cl>
2024-05-08 13:41:43 -0400

    [session] add session users CRUD [#311]

--- ethicapp/backend/api/v2/sessions.js
+++ ethicapp/backend/api/v2/sessions.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser'); // Importa body-parser
 const router = express.Router();
 
 // Import Model
-const { Sessions } = require('../../api/v2/models');
+const { Sessions, SessionsUsers } = require('../../api/v2/models');
 
 router.use(bodyParser.json());
 
@@ -55,6 +55,37 @@ router.delete('/:id', async (req, res) => {
         }
         await session.destroy();
         res.status(204).end();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ status: 'error', message: 'Internal server error' });
+    }
+});
+
+// CRUD operations for SessionsUsers
+
+// Create session user
+router.post('/:sessionId/users', async (req, res) => {
+    const { sessionId } = req.params;
+    try {
+        const session = await Sessions.findByPk(sessionId);
+        if (!session) {
+            return res.status(404).json({ status: 'error', message: 'Session not found' });
+        }
+        console.log("req.body.user_id -> ", req.body.user_id)
+        const sessionUser = await SessionsUsers.create({ sesion_id: sessionId, user_id: req.body.user_id });
+        res.status(201).json({ status: 'success', data: sessionUser });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ status: 'error', message: 'Internal server error' });
+    }
+});
+
+// Read session users
+router.get('/:sessionId/users', async (req, res) => {
+    const { sessionId } = req.params;
+    try {
+        const sessionUsers = await SessionsUsers.findAll({ where: { sesion_id: sessionId } });
+        res.status(200).json({ status: 'success', data: sessionUsers });
     } catch (err) {
         console.error(err);
         res.status(500).json({ status: 'error', message: 'Internal server error' });
