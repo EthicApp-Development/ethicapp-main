0b66e84a6ed05b86f5addbd341128e22d451c5c2
Benjamin Gonzalez <bigonzalez@miuandes.cl>
2024-05-12 19:02:52 -0400

    [api/v2] add phase seed [#312]

--- ethicapp/backend/api/v2/seeders/development/20240512211030-phases.js
+++ ethicapp/backend/api/v2/seeders/development/20240512211030-phases.js
@@ -3,15 +3,44 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('phases', [
+      {
+        number: 1,
+        type: 'Type A',
+        anon: true,
+        chat: false,
+        prev_ans: 'Previous answer A',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 2,
+        type: 'Type B',
+        anon: false,
+        chat: true,
+        prev_ans: 'Previous answer B',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 3,
+        type: 'Type c',
+        anon: true,
+        chat: true,
+        prev_ans: 'Previous answer C',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 4,
+        type: 'Type D',
+        anon: true,
+        chat: true,
+        prev_ans: 'Previous D answer',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
