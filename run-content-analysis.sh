43118e849d3b7880987bc5f0201f9e3711d837c5
Claudio Álvarez Gómez <calvarez1@miuandes.cl>
2024-07-02 20:01:15 +0200

    Content analysis launch script updated, as it was not using the proper docker-compose configuration.

--- run-content-analysis.sh
+++ run-content-analysis.sh
@@ -15,7 +15,7 @@ fi
 
 # Check the value of INCLUDE_NGINX and run docker-compose with the necessary files
 if [ "$INCLUDE_NGINX" = "true" ]; then
-  docker-compose -p "$ETHICAPP_PROJECT_NAME" -f docker-compose.yml -f docker-compose.nginx.yml up
+  docker-compose -p "$ETHICAPP_PROJECT_NAME" -f docker-compose.content-analysis.yml -f docker-compose.nginx.yml up
 else
-  docker-compose -p "$ETHICAPP_PROJECT_NAME" -f docker-compose.yml up
+  docker-compose -p "$ETHICAPP_PROJECT_NAME" -f docker-compose.content-analysis.yml up
 fi
