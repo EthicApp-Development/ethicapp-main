2587417c1e232fd863390d058d9f415779e231f5
francisco-delamaza <fjdelamaza@miuandes.cl>
2024-09-25 20:12:59 -0300

    [api/v2] Creación de scripts para testing y configuracion de ambiente para pruebas API [#369]

--- postgres-db/init-db.sh
+++ postgres-db/init-db.sh
@@ -5,22 +5,30 @@
 # --------------------------------------------------------------------------------------------------
 
 DB_CONNECTION_URI="postgresql://$DB_USER_NAME:$DB_USER_PASSWORD@localhost:5432/$DB_NAME"
+DB_CONNECTION_URI_TEST="postgresql://$DB_TEST_USER_NAME:$DB_TEST_USER_PASSWORD@localhost:5432/$DB_TEST_NAME"
+
 
 /etc/init.d/postgresql start
 
 # Replacing ENV variables at the SQL script
 mv ./create-db.sql /tmp/create-db.sql
-envsubst '${DB_USER_NAME} ${DB_USER_PASSWORD} ${DB_NAME}' < /tmp/create-db.sql > ./create-db.sql
+envsubst '${DB_USER_NAME} ${DB_USER_PASSWORD} ${DB_NAME} ${DB_TEST_USER_NAME} ${DB_TEST_USER_PASSWORD} ${DB_TEST_NAME}' < /tmp/create-db.sql > ./create-db.sql
 
-# Creating database
+# Creating development and test databases
 su postgres -c "psql --set ON_ERROR_STOP=1 --file ./create-db.sql"
 
-# Creating schema
+# Creating schema for development database
 for sqlFile in ./schema/*.sql; do
     psql ${DB_CONNECTION_URI} --set ON_ERROR_STOP=1 --file ${sqlFile}
+done
+
+#Creating schema for test database
+for sqlFile in ./schema/*.sql; do
+    psql ${DB_CONNECTION_URI_TEST} --set ON_ERROR_STOP=1 --file ${sqlFile}
 done
 
 # Populating development database with basic data
 for sqlFile in ./seeds/development/*.sql; do
     psql ${DB_CONNECTION_URI} --set ON_ERROR_STOP=1 --file ${sqlFile}
 done
+
