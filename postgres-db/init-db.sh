436d280bdc1f0abb8076f090e83c5014d0222bf5
francisco-delamaza <fjdelamaza@miuandes.cl>
2024-09-27 12:32:32 -0300

    [api/v2] Correcciones en configuracion de entorno de testing [#369]

--- postgres-db/init-db.sh
+++ postgres-db/init-db.sh
@@ -3,16 +3,13 @@
 # Script for initializing database (schema and development basic data). Intended to be ran at the
 # database's Dockerfile (at build time).
 # --------------------------------------------------------------------------------------------------
-
 DB_CONNECTION_URI="postgresql://$DB_USER_NAME:$DB_USER_PASSWORD@localhost:5432/$DB_NAME"
-DB_CONNECTION_URI_TEST="postgresql://$DB_TEST_USER_NAME:$DB_TEST_USER_PASSWORD@localhost:5432/$DB_TEST_NAME"
-
 
 /etc/init.d/postgresql start
 
 # Replacing ENV variables at the SQL script
 mv ./create-db.sql /tmp/create-db.sql
-envsubst '${DB_USER_NAME} ${DB_USER_PASSWORD} ${DB_NAME} ${DB_TEST_USER_NAME} ${DB_TEST_USER_PASSWORD} ${DB_TEST_NAME}' < /tmp/create-db.sql > ./create-db.sql
+envsubst '${DB_USER_NAME} ${DB_USER_PASSWORD} ${DB_NAME}' < /tmp/create-db.sql > ./create-db.sql
 
 # Creating development and test databases
 su postgres -c "psql --set ON_ERROR_STOP=1 --file ./create-db.sql"
@@ -22,13 +19,7 @@ for sqlFile in ./schema/*.sql; do
     psql ${DB_CONNECTION_URI} --set ON_ERROR_STOP=1 --file ${sqlFile}
 done
 
-#Creating schema for test database
-for sqlFile in ./schema/*.sql; do
-    psql ${DB_CONNECTION_URI_TEST} --set ON_ERROR_STOP=1 --file ${sqlFile}
-done
-
 # Populating development database with basic data
 for sqlFile in ./seeds/development/*.sql; do
     psql ${DB_CONNECTION_URI} --set ON_ERROR_STOP=1 --file ${sqlFile}
 done
-
