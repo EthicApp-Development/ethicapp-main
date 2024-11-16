271372f063749d971bea2608100bd5396592f77c
Claudio Álvarez Gómez <calvarez1@miuandes.cl>
2024-11-15 21:53:12 -0300

    Further adjustments and improvements to display of designs.

--- ethicapp/frontend/assets/js/components/design-description.js
+++ ethicapp/frontend/assets/js/components/design-description.js
@@ -9,7 +9,7 @@ let DesignDescriptionComponent = {
             <div class="panel-body">
                 <p><i class="fa-solid fa-info-circle" aria-hidden="true"></i> {{ 'title_colon' | translate }} <a ng-href="#!/designs/{{$ctrl.design.id}}">{{$ctrl.design.metainfo.title}}</a></p>
                 <p><i class="fa-solid fa-calendar-alt" aria-hidden="true"></i> {{ 'creation_date_colon' | translate }} {{$ctrl.design.metainfo.creation_date | date}}</p>
-                <p><i class="fa-solid fa-paint-brush" aria-hidden="true"></i> {{ 'design_type_colon' | translate }} {{$ctrl.design.type | translate}}</p>
+                <p><i class="fa-solid fa-paint-brush" aria-hidden="true"></i> {{ 'design_type_colon' | translate }} {{$ctrl.design.type === "semantic_differential" ? ("semantic_differential" | translate) : ("ranking_activity" | translate)}}</p>
                 <p><i class="fa-solid fa-layer-group" aria-hidden="true"></i> {{ 'number_of_phases_colon' | translate }} {{$ctrl.design.phases.length}}</p>
             </div>                                    
         </div>
