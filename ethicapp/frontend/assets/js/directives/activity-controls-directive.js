141ebd629da9281167704a9fd98f0eb61f2427b0
Claudio Álvarez Gómez <calvarez1@miuandes.cl>
2024-11-16 23:01:24 -0300

    Improved the activityControls directive.

--- ethicapp/frontend/assets/js/directives/activity-controls-directive.js
+++ ethicapp/frontend/assets/js/directives/activity-controls-directive.js
@@ -10,24 +10,25 @@ let activityControlsDirective = function() {
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-12 text-center">
-                            <!-- Section 1: Main Actions -->                             
-                            <button class="btn btn-default btn-sm" ng-click="parentCtrl.startNextPhase()">
-                                <i class="fa-solid fa-forward text-success"></i> {{ 'start_next_phase_button' | translate }}
-                            </button>
-                            <button class="btn btn-default btn-sm" ng-click="parentCtrl.endActivity()">
-                                <i class="fa-solid fa-stop-circle text-danger"></i> {{ 'end_activity_button' | translate }}
-                            </button>
-            
-                            <!-- Separator -->
-                            <span class="button-separator" ng-if="isFinished">|</span>
+                            <!-- Section 1: Main Actions -->
+                            <div ng-if="!isFinished" class="action-buttons">
+                                <button class="btn btn-default btn-sm" ng-click="parentCtrl.startNextPhase()">
+                                    <i class="fa-solid fa-forward text-success"></i> {{ 'start_next_phase_button' | translate }}
+                                </button>
+                                <button class="btn btn-default btn-sm" ng-click="parentCtrl.endActivity()">
+                                    <i class="fa-solid fa-stop-circle text-danger"></i> {{ 'end_activity_button' | translate }}
+                                </button>
+                            </div>
             
                             <!-- Section 2: Reports -->
-                            <button class="btn btn-default btn-sm" ng-click="parentCtrl.downloadAnswersReport()" ng-if="isFinished">
-                                <i class="fa-solid fa-file-alt text-primary"></i> {{ 'answers_report_button' | translate }}
-                            </button>
-                            <button class="btn btn-default btn-sm" ng-click="parentCtrl.downloadChatLog()" ng-if="isFinished">
-                                <i class="fa-solid fa-comments text-info"></i> {{ 'chat_log_button' | translate }}
-                            </button>
+                            <div ng-if="isFinished" class="report-buttons">
+                                <button class="btn btn-default btn-sm" ng-click="parentCtrl.downloadAnswersReport()">
+                                    <i class="fa-solid fa-file-alt text-primary"></i> {{ 'answers_report_button' | translate }}
+                                </button>
+                                <button class="btn btn-default btn-sm" ng-click="parentCtrl.downloadChatLog()">
+                                    <i class="fa-solid fa-comments text-info"></i> {{ 'chat_log_button' | translate }}
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
