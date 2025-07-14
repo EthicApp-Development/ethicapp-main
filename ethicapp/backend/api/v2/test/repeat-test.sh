88b7d4c21197a3608509c94ed106f3b919ce02a6
francisco-delamaza <fjdelamaza@miuandes.cl>
2025-07-14 09:39:41 -0400

    Se cierran las conexiones de los tests

--- ethicapp/backend/api/v2/test/repeat-test.sh
+++ ethicapp/backend/api/v2/test/repeat-test.sh
@@ -4,7 +4,7 @@ echo "Running tests with NODE_ENV=$NODE_ENV"
 for i in {1..1}
 do
   echo "Running test iteration $i"
-  npx jest --detectOpenHandles
+  npx jest --detectOpenHandles --forceExit
   if [ $? -ne 0 ]; then
     echo "Test failed on iteration $i"
     exit 1
